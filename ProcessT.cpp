18b8dad5b04623f3c67ca8d294eb9d668d752a41
jtran93 <johnathan_tran93@yahoo.com>
2016-02-24 19:35:58 -0600

    Revert "Revert "Event completion""
    This reverts commit ed04f1b8a10ab5c4dfbce4fc1e199ec7d25340ee.

--- ProcessT.cpp
+++ ProcessT.cpp
@@ -3,10 +3,9 @@
 
 void ProcessT::addDataToProcess(DataT data)
 {
-	int dataSize = data.getSize();	
 	int completionT = 0;
 
-	for(int i = 0; i < dataSize; i++)
+	for(int i = 0; i < data.getSize(); i++)
 	{
 		completionT = completionT + data.getTime(i);
 		
@@ -14,16 +13,16 @@ void ProcessT::addDataToProcess(DataT data)
 		{
 			startTime.push_back(data.getTime(i));
 			firstLine.push_back(i);				
-			currentLine.push_back(i);
+			currentLine.push_back(i+1);
 			hasStarted.push_back('N');
 		}
 		else if(data.getOperation(i) == "END")
 		{	
 			lastLine.push_back(i);
-			IOCompletionTime.push_back(completionT);
+			IOCompletionTime.push_back(0);
 			processState.push_back("Waiting");
-			completionT = 0;
 			priority.push_back("High");
+			completionT = 0;
 		}		
 	}
 	
@@ -36,8 +35,7 @@ void ProcessT::printProcessTable(DataT data)
 	int start = 0;
 	int end = 0;
 	
-	int size = startTime.size();
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < startTime.size(); i++)
 	{
 		if(processState[i] != "Done")
 		{
@@ -67,9 +65,9 @@ void ProcessT::removeProcess(int pid)
 	processState[pid] = "Done";
 }
 
-void ProcessT::IORequest(int pid, int clock, int requestTime)
+void ProcessT::IORequest(int pid, int requestTime)
 {
-	IOCompletionTime[pid] = clock + requestTime;
+	IOCompletionTime[pid] = totalElapsedTime + requestTime;
 }
 
 void ProcessT::IOCompletion(int pid)
@@ -82,9 +80,39 @@ void ProcessT::setPriority(int pid, std::string pri)
 	priority[pid] = pri;
 }
 
+void ProcessT::incrementCurrentLine(int pid)
+{
+	currentLine[i]++;
+}
+
 std::vector<int> ProcessT::getStartTime()
 {
 	return startTime;
+}
+
+int ProcessT::getFirstLine(int pid)
+{
+	return firstLine[pid];
+}
+
+int ProcessT::getLastLine(int pid)
+{
+	return lastLine[pid];
+}
+
+int ProcessT::getCurrentLine(int pid)
+{
+	return currentLine[pid];
+}
+
+std::string ProcessT::getProcessState(int pid)
+{
+	return processState[pid];
+}
+
+int ProcessT::getIOCompletionTime(int pid)
+{
+	return IOCompletionTime[pid];
 }
 
 std::string ProcessT::getPriority(int pid)
@@ -94,16 +122,10 @@ std::string ProcessT::getPriority(int pid)
 
 void ProcessT::searchLowestStartTime(int& pid, int& time)
 {
-	int size = startTime.size();
 	
-	for(int i = 0; i < size; i++)
+	for(int i = 0; i < startTime.size(); i++)
 	{
-		if (i == 0)
-		{
-			time = startTime[i];
-			pid = i;
-		}
-		else if(startTime[i]<time && hasStarted[i]=='N')
+		if(startTime[i]<time && hasStarted[i]=='N')
 		{
 			time = startTime[i];
 			pid = i;
@@ -114,16 +136,10 @@ void ProcessT::searchLowestStartTime(int& pid, int& time)
 
 void ProcessT::searchLowestIOTime(int& pid, int& time)
 {
-	int size = IOCompletionTime.size();
 	
-	for(int i = 0; i < size; i++)
+	for(int i = 0; i < IOCompletionTime.size(); i++)
 	{
-		if (i == 0)
-		{
-			time = IOCompletionTime[i];
-			pid = i;
-		}
-		else if(IOCompletionTime[i]<time)
+		if(IOCompletionTime[i] != 0 && IOCompletionTime[i]<time)
 		{
 			time = IOCompletionTime[i];
 			pid = i;
--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 #include "Device.h"
 #include "Global.h"
 
+#include <sstream>
+
 int main()
 {	
-
-	int lowStartTime = 0;
+	std::stringstream ss;
+	std::string str;
+	
+	int nextRequest = -1;
+	int did = 0;
+	int lowStartTime = 500000;
 	int lowStartPID = 0;
-	int lowDeviceTime = 0;
+	int lowDeviceTime = 500000;
 	int lowDevicePID = 0;
-	int lowIOTime = 0;
+	int lowIOTime = 500000;
 	int lowIOPID = 0;
 	
 	DataT data;
@@ -19,46 +25,69 @@ int main()
 	data.readData();
 	process.addDataToProcess(data);
 	
-	data.printData();
 	process.printProcessTable(data);
-	
-	std::cout<<"Deleting a process\n";
-	process.removeProcess(0);
-	process.printProcessTable(data);
-	
-	device.printDevice();
 
 	
-	while(process.getStartTime().empty() != true)
-	{
-
-		
+	//while(process.searchProcessRemaining())
+	//{
 		process.searchLowestStartTime(lowStartPID, lowStartTime);
 		process.searchLowestIOTime(lowIOPID, lowIOTime);
-		device.searchLowestCompletionTime(lowDevicePID, lowDeviceTime);
+		device.searchLowestCompletionTime(lowDevicePID, lowDeviceTime, did);
+		
+		std::cout<<"LowStartPID and LowStartTime: "<<lowStartPID<<", "<<lowStartTime<<"\n";
+		std::cout<<"LowIOPID and LowIOTime: "<<lowIOPID<<", "<<lowIOTime<<"\n";
+		std::cout<<"LowDevicePID and LowDeviceTime: "<<lowDevicePID<<", "<<lowDeviceTime<<"\n";
 		
 		if(lowStartTime < lowIOTime && lowStartTime < lowDeviceTime)
-		{
-			//Arrival event of lowStartPID
+		{//Arrival event of lowStartPID
+		 //At arrival time lowStartPID requests a core
+			ss<<lowStartPID;
+			str = ss.str();
+			
+			totalElapsedTime = data.getTime(process.getFirstLine(lowStartPID));
+			device.CPURequest(process.getFirstLine(lowStartPID)+1, str, process.getPriority(lowStartPID));
+			process.setPriority(lowStartPID, "High");
+			
+			process.incrementCurrentLine(lowStartPID);
+			nextRequest = lowStartPID;
 		}
 		else if (lowIOTime < lowStartTime && lowIOTime < lowDeviceTime)
-		{
-			//Complete IO of lowIOPID and fetch the next operation
+		{//Complete IO of lowIOPID and fetch the next operation
+			ss<<lowIOPID;
+			str = ss.str();
+			
+			process.IOCompletion(str);
+			process.setPriority(lowIOPID, "High");
+			totalTimeElapsed = process.getIOCompletionTime(lowIOPID);
+			
+			process.incrementCurrentLine(lowIOPID);
+			nextRequest = lowIOPID;
 		}
 		else if (lowDeviceTime < lowStartTime && lowDeviceTime < lowIOTime)
-		{
+		{//Complete operation of lowDevicePID and fetch the next operation
+			ss<<lowDevicePID;
+			str = ss.str();
 			
-			//Complete operation of lowDevicePID and fetch the next operation
+			if(did == 0 || did == 1)
+			{
+				device.CPUCompletion(did);
+				totalTimeElapsed = device.getCompletionTime(did);
+			}
+			else if(did == 2)
+			{
+				device.DiskCompletion();
+				totalTimeElapsed = device.getCompletionTime(did);
+				process.setPriority(lowDevicePID, "Low");
+			}
+			
+			process.incrementCurrentLine(lowDevicePID);
+			nextRequest = lowDevicePID;
 		}
 		
-	}
+		
+		
+	//}
 
-	
-	
-	
-	
-	
-	
 	return 0;
 }
 
